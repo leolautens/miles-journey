37f06b345822ef1ab9b6f834866a188290369d02
Leonardo Lautenschlaeger <70241645+leolautens@users.noreply.github.com>
2023-10-15 19:17:38 -0300

    feat(filtros-complementares): add filter event

--- src/app/shared/form-busca/filtros-complementares/filtros-complementares.component.ts
+++ src/app/shared/form-busca/filtros-complementares/filtros-complementares.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuscaService } from 'src/app/core/services/form-busca.service';
 
 @Component({
   selector: 'app-filtros-complementares',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./filtros-complementares.component.scss']
 })
 export class FiltrosComplementaresComponent {
+  @Output() search = new EventEmitter();
+  constructor(
+    private formBuscaService: FormBuscaService
+  ) {
 
+  }
+
+  busca() {
+    if(!this.formBuscaService.IsValid){
+      this.formBuscaService.formBusca.markAllAsTouched()
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      })
+      return 
+    }
+    this.search.emit(this.formBuscaService.obterFiltros());
+  }
 }

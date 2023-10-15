6c2ded7e05cfbdc58d3ec477f8ac16d08b14d95d
Leonardo Lautenschlaeger <70241645+leolautens@users.noreply.github.com>
2023-10-15 18:44:50 -0300

    feat(form-busca): add filtros complementares component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,8 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatCheckboxModule } from '@angular/material/checkbox'; 
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSliderModule } from '@angular/material/slider'; 
 
 import { BotaoControleComponent } from './shared/botao-controle/botao-controle.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
@@ -44,6 +45,11 @@ import { PerfilComponent } from './pages/perfil/perfil.component';
 import { AutenticacaoInterceptor } from './core/interceptors/autenticacao.interceptor';
 import { BuscaComponent } from './pages/busca/busca.component';
 import { PassagemComponent } from './shared/passagem/passagem.component';
+import { FiltrosComplementaresComponent } from './shared/form-busca/filtros-complementares/filtros-complementares.component';
+import { LabelComponent } from './shared/form-busca/filtros-complementares/label/label.component';
+import { ParadasComponent } from './shared/form-busca/filtros-complementares/paradas/paradas.component';
+import { PrecosComponent } from './shared/form-busca/filtros-complementares/precos/precos.component';
+import { CompanhiasComponent } from './shared/form-busca/filtros-complementares/companhias/companhias.component';
  
 
 @NgModule({
@@ -69,7 +75,12 @@ import { PassagemComponent } from './shared/passagem/passagem.component';
     CadastroComponent,
     PerfilComponent,
     BuscaComponent,
-    PassagemComponent
+    PassagemComponent,
+    FiltrosComplementaresComponent,
+    LabelComponent,
+    ParadasComponent,
+    PrecosComponent,
+    CompanhiasComponent
   ],
   imports: [
     BrowserModule,
@@ -91,7 +102,8 @@ import { PassagemComponent } from './shared/passagem/passagem.component';
     MatAutocompleteModule,
     MatRadioModule,
     MatDividerModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSliderModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
